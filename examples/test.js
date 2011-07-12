3408254e46b809d5936d5a25663275d08870bc24
Jae <jlee@yetitrails.com>
2011-07-12 16:40:05 +0100

    fetch & execute implemented - cheers Andy Kent!

---
+++ examples/test.js
@@ -0,0 +1,14 @@
+hive = require('../node-hive').for({server:"hive.hadoop.forward.co.uk"});
+
+hive.fetch("SELECT * FROM weather_data where dated = '2011-07-01' limit 10", function(err, data) {
+  console.log("SELECT * FROM weather_data where dated = '2011-07-01' limit 10");
+  console.log(data);
+});
+hive.fetch("SELECT * FROM weather_data where dated = '2011-07-02' limit 10", function(err, data) {
+  console.log("SELECT * FROM weather_data where dated = '2011-07-02' limit 10");
+  console.log(data)
+});
+hive.execute("DESCRIBE weather_data", function(err, data) {
+  console.log("DESCRIBE weather_data");
+  console.log(data);
+});
---
+++ node-hive.js
@@ -0,0 +1,44 @@
+var thrift = require('thrift'),
+    ttransport = require('thrift/transport'),
+    ThriftHive = require('gen-nodejs/ThriftHive');
+
+var futureConnection = function(config) {
+  return function(cb) {
+    var connection = thrift.createConnection(config.server, config.port || 10000, {transport: ttransport.TBufferedTransport, timeout: config.timeout || 1000});
+    var client = thrift.createClient(ThriftHive, connection);
+    connection.addListener("connect", function(){
+      cb(client, connection);
+    });
+  };
+};
+
+var hiveClient = function(futureConnection) {
+  return {
+    fetch: function(query, cb) {
+      futureConnection(function(client, connection) {
+        client.execute(query, function(err){
+          if (err) return cb(true, err);
+          client.fetchAll(function(err, data){
+            if (err) return cb(true, err);
+            cb(null, data);
+            connection.end();
+          });
+        });
+      })
+    },
+    
+    execute: function(query, cb){
+      futureConnection(function(client, connection) {
+        client.execute(query, function(err){
+          if (err) return cb(true, err);
+          cb(null, null);
+          connection.end();
+        });
+      });
+    },
+  };
+};
+
+exports.for = function(config) {
+  return hiveClient(futureConnection(config));
+};
