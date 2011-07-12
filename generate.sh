51907c2ca0d72c8194f00486ed917fd804ea7e83
Jae <jlee@yetitrails.com>
2011-07-12 14:00:07 +0100

    moved hive-thrift definition into src directory'

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-thrift -o node_modules --gen js:node -I thrift thrift/hive_metastore.thrift
-thrift -o node_modules --gen js:node -I thrift thrift/hive_service.thrift 
-thrift -o node_modules --gen js:node -I thrift thrift/fb303.thrift 
-thrift -o node_modules --gen js:node -I thrift thrift/serde.thrift 
-thrift -o node_modules --gen js:node -I thrift thrift/queryplan.thrift 
+thrift -o node_modules --gen js:node -I src/hive-thrift src/hive-thrift/hive_metastore.thrift
+thrift -o node_modules --gen js:node -I src/hive-thrift src/hive-thrift/hive_service.thrift 
+thrift -o node_modules --gen js:node -I src/hive-thrift src/hive-thrift/fb303.thrift 
+thrift -o node_modules --gen js:node -I src/hive-thrift src/hive-thrift/serde.thrift 
+thrift -o node_modules --gen js:node -I src/hive-thrift src/hive-thrift/queryplan.thrift 
